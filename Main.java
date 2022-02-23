60aef0681264eb63212511e6f49dbfaccadce8de
VladislavKabakov <95039843+VladislavKabakov@users.noreply.github.com>
2022-02-23 23:11:35 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
         double left_boarder = 0, right_boarder = Math.pow(2, 31);
         while (true) {
             double middle = Math.floor((left_boarder + right_boarder) / 2);
-            System.out.printf("Tell if %.1f is less than your number (0) or greater (1) or equal to (2) \n", middle);
+            System.out.printf("Tell if %.0f is less than your number (0) or greater (1) or equal to (2) \n", middle);
             double input = in.nextDouble();
             if (input == 0) {
                 left_boarder = middle - 1;
